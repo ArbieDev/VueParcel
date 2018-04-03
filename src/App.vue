dc592267eb19c19d1011c646bf582e1bd185f4eb
Arbie C <acskaleidoscope@gmail.com>
2018-04-03 23:50:01 +0900

    git init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container" id="app">
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        message: 'Vue.js App is LIVE!'
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  #app {
+    color: green;
+  }
+</style>
